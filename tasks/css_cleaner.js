cf1506327ae08546866a6340fea836508c9fe9b5
Paolo Moretti <littlebrown@gmail.com>
2014-08-15 10:39:19 +0100

    General refactor

--- tasks/css_cleaner.js
+++ tasks/css_cleaner.js
@@ -6,12 +6,7 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
-module.exports = function (grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+module.exports = function (grunt) { 'use strict';
 
   grunt.registerMultiTask('css_cleaner', 'Clean your unused css classes from your app', function () {
 
@@ -24,19 +19,66 @@ module.exports = function (grunt) {
 
     Array.prototype.unique = function () {
       return this.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+    };
+
+    Array.prototype.cleanClasses = function () {
+      for(var i=0; i<this.length; i++) {
+        this[i] = this[i].replace(" {", "");
+      }
+      return this;
+    };
+
+    Array.prototype.clean = function() {
+      var cleaned = [];
+      this.map(function (item) {
+        var rules = [];
+        item.rules.map(function (rule) {
+          if(rule !== null)
+            rules.push(rule);
+        });
+        if(rules.length > 0) {
+          item.rules = rules;
+          cleaned.push(item);
+        }
+      });
+      return cleaned;
+    };
+
+    Array.prototype.printReport = function () {
+      var s = "";
+      this.map(function (item) {
+        s += "\r\n\n=> " + item.file + "\r\n";
+        s += item.rules.join("\r\n")
+      });
+      return s;
+    };
+
+    function notIgnored(rules) {
+      var ret = [];
+      rules.map(function (rule) {
+        var pass = true;
+        options.ignore.map(function (ignore) {
+          if(rule.search(ignore) !== -1) {
+            return pass = false;
+          }
+        });
+        if (pass === true )
+          ret.push(rule);
+      });
+      return ret;
     }
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      writeReport: false
+      writeReport   : false,
+      templatesPath : ["./"],
+      templatesType : ["html"],
+      ignore        : []
     });
 
-    console.log ("bootstrap", this);
-
-    // Iterate over all specified file groups.
-
     var reportContent   = "";
-    var cssRules        = [];
+    var styleRules      = [];
+    var styleRulesObj   = [];
 
     // Find defined rules
     this.files.forEach(function (filePaths) {
@@ -44,73 +86,73 @@ module.exports = function (grunt) {
       filePaths.src.forEach(function (filepath) {
         var content = grunt.file.read(filepath);
 
-        cssRules = cssRules.concat (content.match(/\.[\w-_\d]+/gi));
+        var found = content.match(new RegExp(/(\.[\w-_\d]+)( {)/g));
+        if (found != null) {
+          styleRules = styleRules.concat (notIgnored(found.cleanClasses()));
+          styleRulesObj.push({
+            file: filepath,
+            rules: notIgnored(found.cleanClasses())
+          });
+        }
 
       });
-      cssRules = cssRules.unique();
-      reportContent += "==> "+_this.target+"\r"+filePaths.src+"\n\rFound " + cssRules.length + " css rules in " + filePaths.src.length + " files";
+      styleRules = styleRules.unique();
+
+      reportContent += "== "+_this.target+" ==";
+      reportContent += "\n\rFound " + styleRules.length + " css rules";
+      reportContent += "\n\r" + filePaths.src;
 
     });
 
     // Search code for usage
-    var sources = [];
+    var sources   = [];
     var rulesUsed = [];
-    grunt.file.recurse("./", function (abspath, rootdir, subdir, filename) {
-      if(filename.split(".")[1] == "html") {
 
-        var content = grunt.file.read(abspath);
+    grunt.file.recurse("./app", function (abspath, rootdir, subdir, filename) {
+      var fileExtension = filename.split(".")[filename.split(".").length - 1];
 
-        cssRules.forEach(function (rule) {
-          if (rule !== null) {
-            if (content.indexOf(rule.split(".")[1]) > -1 && rulesUsed.indexOf(rule) == -1) {
-              rulesUsed.push(rule);
+      options.templatesType.forEach( function (templateType) {
+        if (fileExtension == templateType) {
+          var content = grunt.file.read(abspath);
+
+          styleRulesObj.forEach(function (conf, i) {
+            conf.rules.forEach(function (rule, j) {
+              if (rule !== null) {
+                if (content.indexOf(rule.split(".")[1]) > -1) {
+                  if (rulesUsed.indexOf(rule) == -1)
+                    rulesUsed.push(rule);
+
+                  styleRulesObj[i].rules[j] = null;
+                }
+              }
+            });
+          });
+
+          styleRules.forEach(function (rule) {
+            if (rule !== null) {
+              if (content.indexOf(rule.split(".")[1]) > -1 && rulesUsed.indexOf(rule) == -1) {
+                rulesUsed.push(rule);
+              }
             }
-          }
-        });
-        sources.push(abspath);
-      }
+          });
+          sources.push(abspath);
+        }
+      });
     });
+
+    styleRulesObj = styleRulesObj.clean();
+
     reportContent += "\r\n" + sources.length + " sources found";
-    reportContent += "\r\n\n** JUNK CLASSES ("+cssRules.diff(rulesUsed).length+")\r" + cssRules.diff(rulesUsed).join(",\r");
-    reportContent += "\r\n\n** USED CLASSES ("+rulesUsed.length+")\r" + rulesUsed.join(",\r");
-//
-//
-//    for (var usageType in this.data.templates) {
-//      this.data.templates[usageType].forEach(function (template) {
-//        console.log ("template", template);
-//      });
-//    }
+    reportContent += "\r\n\n** JUNK CLASSES ("+styleRules.diff(rulesUsed).length+")\r" + styleRulesObj.printReport();
+    reportContent += "\r\n\n** USED CLASSES ("+rulesUsed.length+")\r"; // + rulesUsed.join(",\r");
 
+    console.log ("\r\nFound", styleRules.diff(rulesUsed).length, "potentially JUNK classes");
 
-//
-//      // Concat specified files.
-//      var src = file.src.filter(function (filepath) {
-//        // Warn on and remove invalid source files (if nonull was set).
-//        if (!grunt.file.exists(filepath)) {
-//          grunt.log.warn('Source file "' + filepath + '" not found.');
-//          return false;
-//        } else {
-//          return true;
-//        }
-//      }).map(function (filepath) {
-//        // Read file source.
-//        return grunt.file.read(filepath);
-//      }).join(grunt.util.normalizelf(options.separator));
-//
-//      // Handle options.
-//      src += options.punctuation;
-//
-//      // Write the destination file.
-//      grunt.file.write(file.dest, src);
-//
-//      // Print a success message.
-//      grunt.log.writeln('File "' + file.dest + '" created.');
-
-
-    console.log(reportContent);
-
-    if (options.writeReport !== false)
+    if (options.writeReport !== false) {
       grunt.file.write(options.writeReport, reportContent);
+
+      console.log ("\r\n+ Report created at", options.writeReport);
+    }
 
   });
 
